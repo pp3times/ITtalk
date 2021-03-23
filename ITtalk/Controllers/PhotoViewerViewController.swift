f692912c3f875b17c3a31b3990df7f883bbaa5ee
Jeremy <than2314@gmail.com>
2021-03-23 17:49:48 +0700

    :tada: Wrap up Getting Started

---
+++ ITtalk/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViewerViewController.swift
+//  ITtalk
+//
+//  Created by Jeremy on 23/3/2564 BE.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ITtalk/Controllers/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  ITtalk
+//
+//  Created by Jeremy on 23/3/2564 BE.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
