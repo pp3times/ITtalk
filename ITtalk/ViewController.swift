14d79afa9a0be3ac88e8c3a9cb41a3dee0f9c2ca
Jeremy <than2314@gmail.com>
2021-03-23 17:33:24 +0700

    :tada: Getting Started

---
+++ ITtalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITtalk
+//
+//  Created by Jeremy on 23/3/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
